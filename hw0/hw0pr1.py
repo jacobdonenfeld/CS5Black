cbf59b9a3c43b404470238cfdd5a2d98247b63f1
jacobdonenfeld <25359163+jacobdonenfeld@users.noreply.github.com>
2017-10-27 15:11:37 -0700

    Revert "Initial commit"
    This reverts commit c2f20b7a6c9b713ccb751d1a32fbcbaf7faae283.

---
+++ hw0/hw0pr1.py
@@ -0,0 +1,28 @@
+# CS5 Gold/Black: Lab 0, Problem 1
+# Filename: hw0pr1.py
+# Name: 
+# Problem description: The four fours challenge!
+
+from math import *
+
+print("Zero is", 4+4-4-4)
+print("One is", 4*4/(4*4))
+print("Two is", sqrt(4) + sqrt(4) + sqrt(4) - 4)
+print("Three is", 4/4 + 4 - sqrt(4))
+print("Four is", 4 + sqrt(4) + sqrt(4) - 4)
+##print("Five is", factorial(4 - sqrt(4)) + 4/sqrt(4))
+print("Six is", 4 + sqrt(4) + 4 - 4)
+print("Seven is", 4 + 4 - 4/4)
+print("Eight is", 4*4 - 4 - 4)
+print("Nine is", 4 + factorial(sqrt(4)) + 4/sqrt(4))
+print("Ten is", 4*4 - 4 - sqrt(4))
+#print("Eleven is", 4*4/sqrt(4) + )
+print("Twelve is", 4*4 -( sqrt(4) + sqrt(4)))
+#print("Thirteen is", 4*4)
+print("Fourteen is",4+4+4+sqrt(4))
+#print("Fifteen is", )
+print("Sixteen is", 4*4 - 4 + 4)
+print("Seventeen is", 4 * 4 + 4/4)
+print("Eighteen is", 4 * 4 + 4/sqrt(4))
+print("Nineteen is", factorial(4) - 4 - 4/4)
+print("Twenty is", factorial(4) - sqrt(4) - sqrt(4))
