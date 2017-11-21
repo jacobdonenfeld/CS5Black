437bdfff2a38cbcd06f6f9bae9c3c843a6913b8d
jacobdonenfeld <25359163+jacobdonenfeld@users.noreply.github.com>
2017-11-20 20:58:27 -0800

    homework

--- Hw11/hw11pr2/Board.py
+++ Hw11/hw11pr2/Board.py
@@ -31,27 +31,118 @@ class Board:
         s += '\n'
         return s       # the board is complete, return it
 
+    #TODO ask prof ran if this works: for i in (self.data[::-1]):
     def addMove(self, col, ox):
         """ Add the game piece ox (either 'X' or 'O') to column col. """
+        VariableNameNumberOne = len(self.data)
+        for i in range(VariableNameNumberOne):
+            if self.data[VariableNameNumberOne - 1 - i][col] == " ":
+                self.data[VariableNameNumberOne - 1 - i][col] = ox
+                break
+
 
     def clear(self):
         """ Clear the game board of all game pieces. """
+        #TODO ask Ran if I can create a new board obj and set the pointer previously used to it
+        self.data = [[' '] * width for r in range(height)]
 
     def setBoard(self, moves):
         """ Set the board using an input string representation. """
+        #0 = x, 1 = o
+        toggle = 0
+        for i in range(len(moves)):
+            if toggle == 0:
+                self.addMove(int(moves[i]), "X")
+                toggle = 1
+            else:
+                self.addMove(int(moves[i]), "O")
+                toggle = 0
+
 
     def allowsMove(self, col):
         """ Return True if adding a game piece in the given column is 
             permitted and return False otherwise. """
+        if self.data[0][col] == " ":
+            return True
+        return False
 
     def isFull(self):
         """ Return True if the game board is full and False otherwise. """
+        for i in self.data[0]:
+            if i == " ":
+                return False
+        return True
 
     def delMove(self, col):
         """ Delete the topmost game piece from the given column. """
+        for i in range(self.data):
+            if self.data[i][col] != " ":
+                self.data[i][col] = " "
+
 
     def winsFor(self, ox):
         """ Return True if the game has been won by player ox where ox
             is either 'X' or 'O'. """
+        #TODO figure out why this is still out of range
+        for i in range(len(self.data)):
+            for j in range(len(self.data[0])):
+                if self.data[i][j] == ox:
+                    for u in range(3):
+                        if i+u+1 == self.width:
+                            break
+                        if self.data[i+u+1][j] != ox:
+                            break
+                        return True
+                    for u in range(3):
+                        if i-u-1 == -1:
+                            break
+                        if self.data[i-u-1][j] != ox:
+                            break
+                        return True
+                    for u in range(3):
+                        if j+u+1 == self.height:
+                            break
+                        if self.data[i][j+u+1] != ox:
+                            break
+                        return True
+                    for u in range(3):
+                        if j-u-1 == -1:
+                            break
+                        if self.data[i][j-u-1] != ox:
+                            break
+                        return True
+                    #Diagonoals
+                    for u in range(3):
+                        if i+u+1 == self.width or j+u+1 == self.height:
+                            break
+                        if self.data[i+u+1][j+u+1] != ox:
+                            break
+                        return True
+                    for u in range(3):
+                        if i-u-1 == -1 or j-u-1 == -1:
+                            break
+                        if self.data[i-u-1][j-u-1] != ox:
+                            break
+                        return True
+                    for u in range(3):
+                        if i-u-1 == -1 or j+u+1 == self.height:
+                            break
+                        if self.data[i-u-1][j+u+1] != ox:
+                            break
+                        return True
+                    for u in range(3):
+                        if i+u+1 == self.width or j-u-1 == -1:
+                            break
+                        if self.data[i+u+1][j-u-1] != ox:
+                            break
+                        return True
+        return False
+
+b = Board(7,6)
+b.setBoard( '01020305' )
+print(b)
+#print(b.winsFor("X"))
+print(b.winsFor("O"))
+
 
 
